8957937e64b1b61c3e3ee75ec73b24aad572eb62
Manojkumar886 <maddymano434@gmail.com>
2023-12-28 16:07:26 +0530

    Array methofd is overr

---
+++ JAVASCRIPT/functions.js
@@ -0,0 +1,16 @@
+function Addition(a ,b) // a=10 ,b=100
+{
+    let ab=a+b;//110
+    alert(" a value is "+a+" b value is "+b+" a + b ="+ab)
+} 
+
+// fat arrow function -syntex
+//  keyword function=()=>
+//  {
+
+//  }
+
+let myfirstfatarrow=(name)=>
+{
+    alert(" my full name is :"+name);
+}
